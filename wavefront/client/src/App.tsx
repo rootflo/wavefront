6d8d9652c629ad362038827c04746f7c94c68a61
rootflo-hardik <hardik@rootflo.ai>
2025-12-12 18:29:32 +0530

    toast for error/success message

--- wavefront/client/src/App.tsx
+++ wavefront/client/src/App.tsx
@@ -1,10 +1,12 @@
 import { TOKEN_KEY } from "@app/lib/constants";
 import { useEffect } from "react";
 import AppRouter from "./router";
-import { useAuthStore } from "./store";
+import { useAuthStore, useNotifyStore } from "./store";
+import Toast from "./components/ui/toast";
 
 function App() {
   const { setAuthenticatedState } = useAuthStore();
+  const { visible, reset, type, message } = useNotifyStore();
 
   useEffect(() => {
     const token = localStorage.getItem(TOKEN_KEY);
@@ -15,6 +17,7 @@ function App() {
   return (
     <div className="flex h-full w-screen flex-col items-center justify-center bg-white">
       <AppRouter />
+      <Toast visible={visible} reset={reset} type={type} message={message} />
     </div>
   );
 }
---
+++ wavefront/client/src/assets/icons/toast-icons.tsx
@@ -0,0 +1,47 @@
+export const ToastSuccessIcon = () => {
+  return (
+    <div className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+      <svg
+        className="h-5 w-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+      </svg>
+    </div>
+  );
+};
+
+export const ToastErrorIcon = () => {
+  return (
+    <div className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-red-100 text-red-500 dark:bg-red-800 dark:text-red-200">
+      <svg
+        className="h-5 w-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+      </svg>
+    </div>
+  );
+};
+
+export const ToastWarningIcon = () => {
+  return (
+    <div className="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-orange-100 text-orange-500 dark:bg-orange-700 dark:text-orange-200">
+      <svg
+        className="h-5 w-5"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+      >
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" />
+      </svg>
+    </div>
+  );
+};
