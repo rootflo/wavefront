69cf0ef3ef6f0b010d78c62403305140b573caa2
rootflo-hardik <hardik@rootflo.ai>
2026-07-02 14:01:41 +0530

    same default queue for producer also

--- wavefront/server/modules/agents_module/agents_module/utils/celery_client.py
+++ wavefront/server/modules/agents_module/agents_module/utils/celery_client.py
@@ -7,4 +7,6 @@ def get_celery_client() -> Celery:
     broker_url = os.getenv('CELERY_BROKER_URL')
     if not broker_url:
         raise RuntimeError('Missing required env var: CELERY_BROKER_URL')
-    return Celery('async_executor', broker=broker_url)
+    app = Celery('async_executor', broker=broker_url)
+    app.conf.task_default_queue = '{celery}'
+    return app
