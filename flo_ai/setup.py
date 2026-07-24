f9ee9cdead99dbebb5db017065dd0dcc8631212d
rootflo-hardik <hardik@rootflo.ai>
2026-07-24 16:37:49 +0530

    upgraded flo_ai to 1.1.8 (#323)

--- flo_ai/setup.py
+++ flo_ai/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='flo-ai',
-    version='1.1.7',
+    version='1.1.8',
     author='Rootflo',
     description='Create composable AI agents',
     long_description=long_description,
